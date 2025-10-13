d9b5aef8ace4197b64702570b6126682926d4941
emptist <jigme1968@gmail.com>
2025-10-13 20:40:20 +0800

    [SYNC] Automated deployment from mother_repo

--- pages/bull_table.py
+++ pages/bull_table.py
@@ -147,7 +147,7 @@ def prepare_group(symbols_set, interval, pe_limit):
         sort_by_interval=interval,
         all_intervals=[interval],
         pe_limit=pe_limit,
-        due_symbols={},
+        due_symbols={'GDX','GDXU','GLD','UGL'},
     )
     return stk_group
 
