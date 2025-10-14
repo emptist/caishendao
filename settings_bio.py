57c6f81ac0a5e8e4be1dba902edc3df205e98a00
emptist <jigme1968@gmail.com>
2025-10-14 17:03:50 +0800

    clean

--- settings_bio.py
+++
@@ -1,3 +0,0 @@
-# Total 35 with interval=1h c_limit=50 and csvl_limit=3
-# By running on csv file bio.csv: $: python get_consis_etfs.py
-indices = {'IMTX', 'TERN', 'RAPT', 'ACTU', 'VTGN', 'CALC', 'BRNS', 'ZBIO', 'ARWR', 'IVVD', 'VYGR', 'REPL', 'OTLK', 'MLYS', 'TIL', 'INSM', 'ZNTL', 'CYTK', 'PLX', 'RLMD', 'ACRS', 'IBRX', 'BIVI', 'MAZE', 'IONS', 'IRD', 'OSRH', 'ONC', 'LRMR', 'LXRX', 'LCTX', 'ABUS', 'VTVT', 'ATYR', 'APLM'}
