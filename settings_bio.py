77a34095ac70a1a104110ca4d96826b3031ab973
emptist <jigme1968@gmail.com>
2025-10-14 15:58:01 +0800

    [SYNC] Automated deployment from mother_repo

--- settings_bio.py
+++ settings_bio.py
@@ -1,14 +1,3 @@
-
-bios = {
-    'IONS', 'ONC', 'IFRX', 'NRXP', 'YDES', 
-    'PLX', 'APLM', 'CYTK', 'IVVD', 'FBIO', 
-    'LXRX', 'BRNS', 'ATYR', 'ALLR', 'VTGN', 
-    'CARM', 'LRMR', 'CALC', 'IRD', 'PGEN', 
-    'BIVI', 'ARWR', 'LIMN', 'REPL', 'IBRX', 
-    'OTLK', 'MENS', 'ANL', 'ACTU', 'RLMD', 
-    'INSM', 'ZBIO', 'IMTX', 'PMCB', 'ABUS', 
-    'ALEC', 'VYGR', 'CVM', 'LCTX', 'KALA', 
-    'OSRH', 'VTVT', 'GOSS', 'ACRS', 'RAPT', 
-    'MAZE', 'RNXT', 'NBY', 'ZNTL', 'TERN', 
-    'STRO', 'IMRX', 'HOTH', 'TIL', 'MNKD', 'MLYS'
-}
+# Total 35 with interval=1h c_limit=50 and csvl_limit=3
+# By running on csv file bio.csv: $: python get_consis_etfs.py
+indices = {'IMTX', 'TERN', 'RAPT', 'ACTU', 'VTGN', 'CALC', 'BRNS', 'ZBIO', 'ARWR', 'IVVD', 'VYGR', 'REPL', 'OTLK', 'MLYS', 'TIL', 'INSM', 'ZNTL', 'CYTK', 'PLX', 'RLMD', 'ACRS', 'IBRX', 'BIVI', 'MAZE', 'IONS', 'IRD', 'OSRH', 'ONC', 'LRMR', 'LXRX', 'LCTX', 'ABUS', 'VTVT', 'ATYR', 'APLM'}
