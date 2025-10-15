64d21e2b28824fffaa6b7a5a4a4059fb7dab7a02
emptist <jigme1968@gmail.com>
2025-10-15 09:24:09 +0800

    [SYNC] Automated deployment from mother_repo

--- illustration.py
+++ illustration.py
@@ -429,7 +429,7 @@ def bokeh_draw(
             ("velo", "@velo7"),
             ("lcmah7", "@lcmah7"),
             ("cmah7", "@cmah7"),
-            ("cmal7", "@cmal7"),
+            #("cmal7", "@cmal7"),
             ("bbu", "@bbu"),
             # ('bb6u','@bb6u'),
             # ('bb4u','@bb4u'),
